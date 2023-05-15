786e49b4c83631e9601c6e4e5a8fa8d56b607df1
AndrisOrna <andris260379@gmail.com>
2023-05-15 17:56:27 +0100

    Methods
    Adding, multiplying, deviding numbers

--- UdemyCourseCSharp/Program.cs
+++ UdemyCourseCSharp/Program.cs
@@ -1,18 +1,24 @@
-﻿using System;
-namespace HelloWorld
-{
-	class Program
-	{
-		static void Main(string[] args)
-		{
-		WriteSomething();
-
-        }
-		// access modifier (static) return type method name(parameter 1, parameter2)
-		public static void WriteSomething()
-		{
-			Console.WriteLine("Hello");
-			Console.Read();
-		}
-	}
-}
+﻿//using System;
+//namespace HelloWorld
+//{
+//	class Program
+//	{
+//		static void Main(string[] args)
+//		{
+//			WriteSomething();
+//			WriteSomethingSpecific("My name");
+//            Console.Read();
+//        }
+//		// access modifier (static) return type method name(parameter 1, parameter2)
+//		public static void WriteSomething()
+//		{
+//			Console.WriteLine("Hello, i am called from method");
+//			//Console.Read(); // need to call from main method
+//		}
+//		public static void WriteSomethingSpecific(string myText) 
+//		{
+//			Console.WriteLine(myText);
+//			//Console.Read();
+//		}
+//	}
+//}
