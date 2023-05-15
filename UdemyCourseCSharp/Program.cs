55e0883f8ee0e4a9099635b17f924167fb90236b
AndrisOrna <andris260379@gmail.com>
2023-05-15 17:17:48 +0100

    First code
    Setup folder, Methods.

---
+++ UdemyCourseCSharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace HelloWorld
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		WriteSomething();
+
+        }
+		// access modifier (static) return type method name(parameter 1, parameter2)
+		public static void WriteSomething()
+		{
+			Console.WriteLine("Hello");
+			Console.Read();
+		}
+	}
+}
